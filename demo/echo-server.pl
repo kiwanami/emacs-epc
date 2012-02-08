b1f4532e9b5c5ea7992c95eff96a823e03343c79
SAKURAI Masashi <m.sakurai@kiwanami.net>
2012-02-09 00:05:09 +0900

    updated demo

--- demo/echo-server.pl
+++ demo/echo-server.pl
@@ -4,17 +4,17 @@ use RPC::EPC::Service;
 
 sub echo_test {
     my $methods = {
-        'echo' => sub {
+        'echo' => [sub {
             my $args = shift;
             return $args;
-        },
+        },"args","just echo back arguments."],
         'add' => sub {
             my $args_ref = shift;
             my ($a,$b) = @$args_ref;
             return $a + $b;
         }
     };
-    my $server = RPC::EPC::Service->new(8888, $methods);
+    my $server = RPC::EPC::Service->new(0, $methods);
     $server->start;
 }
 
