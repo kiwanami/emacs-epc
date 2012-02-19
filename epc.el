52d68d32e89e6dbc48f191ac8685591d9f86d0ca
SAKURAI Masashi <m.sakurai@kiwanami.net>
2012-02-19 23:34:15 +0900

    update documents

--- epc.el
+++ epc.el
@@ -20,7 +20,14 @@
 
 ;;; Commentary:
 
-;; 
+;; This program is an asynchronous RPC stack for Emacs.  Using this
+;; RPC stack, the Emacs can communicate with the peer process.
+;; Because the protocol is S-expression encoding and consists of
+;; asynchronous communications, the RPC response is fairly good.
+;;
+;; Current implementations for the EPC are followings:
+;; - epcs.el : Emacs Lisp implementation
+;; - RPC::EPC::Service : Perl implementation
 
 ;;; Code:
 
@@ -69,11 +76,12 @@
 
 (defvar epc:accept-process-timeout 100 "[internal] msec")
 
-;; epc:connection structure
-;;   name  : 
-;;   process : 
-;;   buffer : 
-;;   channel  :
+;; [epc:connection structure]
+;;
+;;   name    : Connection name. This name is used for process and buffer names.
+;;   process : Connection process object.
+;;   buffer  : Working buffer for the incoming data.
+;;   channel : Event channels for incoming messages.
 
 (defstruct epc:connection name process buffer channel)
 
@@ -211,7 +219,8 @@ This is more compatible with the CL reader."
 ;;==================================================
 ;; High Level Interface
 
-;; epc:manager
+;; [epc:manager]
+;;
 ;;   title      : instance name for displaying on the `epc:controller' UI
 ;;   server-process : process object for the peer
 ;;   commands   : a list of (prog . args)
@@ -222,7 +231,8 @@ This is more compatible with the CL reader."
 ;;   exit-hook  : functions for after shutdown EPC connection
 (defstruct epc:manager title server-process commands port connection methods sessions exit-hooks)
 
-;; epc:method
+;; [epc:method]
+;;
 ;;   name       : method name (symbol)   ex: 'test
 ;;   task       : method function (function with one argument)
 ;;   arg-specs  : arg-specs (one string) ex: "(A B C D)"
